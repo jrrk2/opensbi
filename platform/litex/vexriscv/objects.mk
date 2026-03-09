2725d4404a2a6d1630c1448de8fb4b7645a4df4a
Jonathan Kimmitt <jonathan@kimmitt.uk>
2026-03-09 08:16:36 +0000

    Merge sonata changes into fork of 1.3.1-linux-on-litex-vexriscv

--- platform/litex/vexriscv/objects.mk
+++ platform/litex/vexriscv/objects.mk
@@ -17,17 +17,26 @@ platform-objs-y += platform.o
 # Command for platform specific "make run"
 platform-runcmd = echo LiteX/VexRiscv SMP
 
-PLATFORM_RISCV_XLEN = 32
-PLATFORM_RISCV_ABI = ilp32
-PLATFORM_RISCV_ISA = rv32ima
-PLATFORM_RISCV_CODE_MODEL = medany
+#PLATFORM_RISCV_XLEN = 32
+#PLATFORM_RISCV_ABI = ilp32
+# openSBI is able to detect if zicsr_zifenci must be
+# added, but without an explicit PLATFORM_RISCV_ISA
+# default is imafdc. So fails with default configuration
+ifeq ($(CC_SUPPORT_ZICSR_ZIFENCEI), y)
+	PLATFORM_RISCV_ISA = rv$(PLATFORM_RISCV_XLEN)ima_zicsr_zifencei
+else
+	PLATFORM_RISCV_ISA = rv$(PLATFORM_RISCV_XLEN)ima
+endif
+#PLATFORM_RISCV_ISA = rv32ima
+#PLATFORM_RISCV_CODE_MODEL = medany
 
 # Blobs to build
-FW_TEXT_START=0x40F00000
+# Sonata XIP: firmware text in flash, kernel XIP from flash base
+FW_TEXT_START=0x02540000
 FW_DYNAMIC=y
 FW_JUMP=y
-FW_JUMP_ADDR=0x40000000
-FW_JUMP_FDT_ADDR=0x40EF0000
+FW_JUMP_ADDR=0x02000000
+FW_JUMP_FDT_ADDR=0x40770000
 FW_PAYLOAD=y
 FW_PAYLOAD_OFFSET=0x40000000
-FW_PAYLOAD_FDT_ADDR=0x40EF0000
+FW_PAYLOAD_FDT_ADDR=0x40770000
